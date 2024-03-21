dd175f356cce002f8e87b7a7ad27979da8dc9889
Muhammad Haseeb <muhammmadhaseebisb@gmail.com>
2024-03-21 10:04:53 +0500

    added new function

--- print-data.go
+++ print-data.go
@@ -2,6 +2,7 @@ package gocorelib
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/apimatic/go-core-runtime/https"
 )
@@ -12,4 +13,18 @@ func PrintString(str string) {
 
 func GetFile(fileUrl string) (https.FileWrapper, error){
 	return https.GetFile(fileUrl)
+}
+
+
+func MergePath(left, right string) string {
+	if right == "" {
+		return left
+	}
+	if left[len(left)-1] == '/' && right[0] == '/' {
+		return left + strings.Replace(right, "/", "", -1)
+	} else if left[len(left)-1] == '/' || right[0] == '/' {
+		return left + right
+	} else {
+		return left + "/" + right
+	}
 }
